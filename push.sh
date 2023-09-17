ca5ce5ee7c9f13ef7697ee18c5a6dd70a0445754
yujingyi11 <xiaoyuooooo@qq.com>
2023-09-17 14:31:12 +0800

    第二次作业

---
+++ push.sh
@@ -0,0 +1,6 @@
+git config --global user.name 'yujingyi11'
+git config --global user.email 'xiaoyuooooo@qq.com'
+git config --global http.sslverify false
+git add .
+git commit -m $'zi'自动推送'
+git push
